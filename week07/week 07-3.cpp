8efd4cca66e161404d062023c5ff884e7d2415b8
陳彥霖 <91170836+william0405@users.noreply.github.com>
2021-10-27 12:01:55 +0800

    Add files via upload

---
+++ week07/week 07-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n=2;
+
+    while(n>0){
+
+        printf("n¤j©ó0\n");
+        n=n-1;
+    }
+}
---
+++ week07/week07-2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n=10;
+//    if(n>0)
+ //   {
+   //     printf("n大於0\n");
+  //  }
+    while(n>0){
+
+        printf("n大於0\n");
+    }
+
+
+}
+
